c3235cab42765265c1a1c85b1d0d2987f1ab5d99
Mo2men-dev <momensameh5@gmail.com>
2025-11-23 07:45:37 +0200

    feat: enhance InputText component with optional props for min/max and className

--- src/components/ui-components/InputText.tsx
+++ src/components/ui-components/InputText.tsx
@@ -4,23 +4,31 @@ function TextInput({
 	name,
 	value,
 	placeholder,
+	className,
+	min = 1,
+	max = 25,
 	onChange,
 }: {
-	label: string;
+	label?: string;
 	name: string;
 	type?: "text" | "password" | "email";
 	value?: string;
 	placeholder: string;
+	className?: string;
+	min?: number;
+	max?: number;
 	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
 	return (
 		<label className="flex flex-col gap-2">
-			<span className="font-semibold">{label}</span>
+			{label && <span className="font-semibold">{label}</span>}
 			<input
-				className="outline-none p-2 rounded-lg border-2 bg-dark-bg text-sm border-dark-border focus:border-dark-tertiary"
+				className={`outline-none p-2 ${className ? className : "rounded-lg border-2 text-sm"} bg-dark-bg border-dark-border focus:border-dark-tertiary`}
 				type={type}
 				name={name}
 				value={value}
+				min={min}
+				max={max}
 				placeholder={placeholder}
 				onChange={onChange}
 			/>
